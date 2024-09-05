fc6fc51766e4c99d014da18f694b0fd7b7c25198
chrioniep <eponde26@gmail.com>
2024-09-05 12:57:29 +0100

    update the work section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -369,16 +369,16 @@ export default function Home() {
         <div className="flex min-h-[6rem] min-w-[18rem] max-w-8xl flex-wrap items-center justify-center">
           <div className="carousel carousel-start">
             <WorkLine
+              bg={"/works/work-1.jpeg"}
+              link={"https://www.logozanaat.app/"}
+            />
+            <WorkLine
               bg={"/works/work-6.jpeg"}
               link={"https://casezanaat.vercel.app/"}
             />
             <WorkLine
               bg={"/works/work-3.jpeg"}
               link={"https://meteorite-strike.web.app/"}
-            />
-            <WorkLine
-              bg={"/works/work-4.jpeg"}
-              link={"https://lisapo-studio.web.app/"}
             />
             <WorkLine
               bg={"/works/work-5.jpeg"}
